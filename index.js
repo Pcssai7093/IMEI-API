373d0ff82b290d7bbbd1756f8790f1d28d93f655
raja <pcssai7093@gmail.com>
2022-05-08 12:05:37 +0530

    commited

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = module.require("express");
 const app = express();
+app.set('view engine','ejs');
+const bodyparser=module.require('body-parser');
+
+app.use(bodyparser.urlencoded({extended:true}));
+
 
 app.listen(3000, () => {
   console.log("server started at 3000");
+});
+
+app.get('/',(req,res)=>{
+  res.render('home');
 });
 
 //  IMEI API
@@ -10,13 +19,12 @@ app.listen(3000, () => {
 app.get("/VerifyIMEI/:imei", (req, res) => {
   const imei = req.params.imei;
   let flag = false;
-  console.log(imei.length);
   let sum = 0;
 
   if (imei.length == 15) {
     for (let i = 0; i < imei.length - 1; i++) {
+
       if (i % 2 == 0) {
-        console.log(imei.charAt(i));
         sum = sum + parseInt(imei.charAt(i));
       } else {
         let digit = parseInt(imei.charAt(i) * 2);
@@ -42,5 +50,45 @@ app.get("/VerifyIMEI/:imei", (req, res) => {
   } else {
     res.send("INVALID IMEI");
   }
+
+});
+
+
+app.post('/VerifyIMEIUI',(req,res)=>{
+  // res.send(req.body);
+  const imei = req.body.Imei;
+  let flag = false;
+  let sum = 0;
+
+  if (imei.length == 15) {
+    for (let i = 0; i < imei.length - 1; i++) {
+
+      if (i % 2 == 0) {
+        sum = sum + parseInt(imei.charAt(i));
+      } else {
+        let digit = parseInt(imei.charAt(i) * 2);
+        while (digit != 0) {
+          sum += digit % 10;
+          digit = Math.floor(digit / 10);
+        }
+      }
+    }
+    let ld = imei.charAt(imei.length - 1);
+    sum += parseInt(ld);
+
+    if (sum % 10 == 0) {
+      flag = true;
+    }
+  } else {
+    flag = false;
+    res.send("invalid imei");
+  }
+
+  if (flag == true) {
+    res.send("VALID IMEI");
+  } else {
+    res.send("INVALID IMEI");
+  }
+
 
 });
---
+++ readme.txt
@@ -0,0 +1,15 @@
+commands and info
+
+RUN the server:
+run the index.js file
+
+To validate IMEI:
+go to localhost:3000/VerifyIMEI/Your_IMEI
+
+note:
+1)To make testing easy IMEI is passed in URL for validation.
+2)Give input IMEI as a parameter in place of "Your_IMEI".
+3)On sending a request validation response will be displayed on the browser.
+
+example request after running the server
+http://localhost:3000/VerifyIMEI/866407051749463
